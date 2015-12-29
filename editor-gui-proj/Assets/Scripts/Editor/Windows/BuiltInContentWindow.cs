86b72ab840e6aec8ff7396051ac6c255523b1aef
Taras Leskiv <leskiv.taras@gmail.com>
2015-12-29 02:57:52 +0100

    added search box implementation to built-in window

--- editor-gui-proj/Assets/Scripts/Editor/Windows/BuiltInContentWindow.cs
+++ editor-gui-proj/Assets/Scripts/Editor/Windows/BuiltInContentWindow.cs
@@ -6,7 +6,7 @@ namespace Leskiv.EditorGuiElements
 {
     public class BuiltInContentWindow : EditorWindow
     {
-        internal static class Styles
+        private static class StylesFromAnimationWindow
         {
             public static GUIContent playContent = EditorGUIUtility.IconContent("Animation.Play");
             public static GUIContent recordContent = EditorGUIUtility.IconContent("Animation.Record");
@@ -19,12 +19,20 @@ namespace Leskiv.EditorGuiElements
             public static GUIStyle miniToolbarButton = new GUIStyle(EditorStyles.toolbarButton);
             public static GUIStyle toolbarLabel = new GUIStyle(EditorStyles.toolbarPopup);
 
-            static Styles()
+            static StylesFromAnimationWindow()
             {
                 toolbarLabel.normal.background = null;
                 miniToolbarButton.padding.top = 0;
                 miniToolbarButton.padding.bottom = 3;
             }
+        }
+
+        private static class BuiltInStyles
+        {
+            public static GUIStyle box = "OL Box";
+            public static GUIStyle title = "OL title";
+            public static GUIStyle searchField = GetStyle("ToolbarSeachTextField");
+            public static GUIStyle cancelSearchButton = GetStyle("ToolbarSeachCancelButton");
         }
 
         [MenuItem("Editor Gui Elements/Built In Stuff")]
@@ -37,18 +45,55 @@ namespace Leskiv.EditorGuiElements
         private void OnGUI()
         {
             DrawAnimationWindowButtons();
+            DrawSearchField();
+            DrawBuiltInStyles();
+        }
+
+        private static void DrawSearchField()
+        {
+            var position = EditorGUILayout.GetControlRect();
+            position.width -= 14f;
+            EditorGUI.TextField(position, "Search Bar Title", "Search Text", BuiltInStyles.searchField);
+            position.width += 14f;
+            position.x += position.width - 14f;
+            position.width = 14f;
+            GUI.Button(position, "", BuiltInStyles.cancelSearchButton);
         }
 
         private static void DrawAnimationWindowButtons()
         {
-            EditorGUILayout.BeginHorizontal(Styles.miniToolbar, GUILayout.MinHeight(15f));
-            GUILayout.Button(BuiltInContentWindow.Styles.playContent, Styles.miniToolbarButton);
-            GUILayout.Button(BuiltInContentWindow.Styles.recordContent, Styles.miniToolbarButton);
-            GUILayout.Button(BuiltInContentWindow.Styles.nextKeyContent, Styles.miniToolbarButton);
-            GUILayout.Button(BuiltInContentWindow.Styles.prevKeyContent, Styles.miniToolbarButton);
-            GUILayout.Button(BuiltInContentWindow.Styles.addKeyframeContent, Styles.miniToolbarButton);
-            GUILayout.Button(BuiltInContentWindow.Styles.addEventContent, Styles.miniToolbarButton);
+            EditorGUILayout.BeginHorizontal(StylesFromAnimationWindow.miniToolbar, GUILayout.MinHeight(15f));
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.playContent, StylesFromAnimationWindow.miniToolbarButton);
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.recordContent, StylesFromAnimationWindow.miniToolbarButton);
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.nextKeyContent, StylesFromAnimationWindow.miniToolbarButton);
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.prevKeyContent, StylesFromAnimationWindow.miniToolbarButton);
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.addKeyframeContent, StylesFromAnimationWindow.miniToolbarButton);
+            GUILayout.Button(BuiltInContentWindow.StylesFromAnimationWindow.addEventContent, StylesFromAnimationWindow.miniToolbarButton);
             EditorGUILayout.EndHorizontal();
+        }
+
+        private static void DrawBuiltInStyles()
+        {
+            EditorGUILayout.BeginHorizontal(BuiltInStyles.title);
+            EditorGUILayout.LabelField("OL title");
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal(BuiltInStyles.box);
+            EditorGUILayout.LabelField("OL Box");
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private static GUIStyle GetStyle(string styleName)
+        {
+            GUIStyle gUIStyle = GUI.skin.FindStyle(styleName);
+            if (gUIStyle == null)
+            {
+                gUIStyle = EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector).FindStyle(styleName);
+            }
+            if (gUIStyle == null)
+            {
+                Debug.LogError("Missing built-in guistyle " + styleName);
+            }
+            return gUIStyle;
         }
     }
 }
